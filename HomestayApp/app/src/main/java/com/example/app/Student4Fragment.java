27c8f42294e3d4bae9a437b56d14e7283a396090
kzumi13 <kevkusumi@yahoo.com>
2014-06-10 04:50:49 -0700

    added cutis's changes

--- HomestayApp/app/src/main/java/com/example/app/Student4Fragment.java
+++ HomestayApp/app/src/main/java/com/example/app/Student4Fragment.java
@@ -1,29 +1,86 @@
 package com.example.app;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.Calendar;
+import java.util.Map;
+import java.util.List;
 
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Student4Fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Student4Fragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+
+
+
+
 public class Student4Fragment extends Fragment {
+
+    View rootView;
+    private ListView lv_sPlacementWizard;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student4, container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_student4, container, false);
+
+        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.simplerow);
+
+        // Find the ListView resource.
+
+        lv_sPlacementWizard = (ListView) rootView.findViewById( R.id.lv_sPlacementWizard );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, planetList);
+
+        // Add more planets. If you passed a String[] instead of a List<String>
+        // into the ArrayAdapter constructor, you must not add more items.
+        // Otherwise an exception will occur.
+        //listAdapter.add( "Ceres" );
+        //listAdapter.add( "Pluto" );
+        //listAdapter.add( "Haumea" );
+        //listAdapter.add( "Makemake" );
+        //listAdapter.add( "Eris" );
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        lv_sPlacementWizard.setAdapter( listAdapter );
+
+        return rootView;
     }
 }
